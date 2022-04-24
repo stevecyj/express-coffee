f45301d02e15eb62e9ae066933d0424b8029c6e9
root <root@localhost.localdomain>
2022-04-24 19:02:50 +0800

    [chore]cors

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
+var cors = require("cors");
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
@@ -15,6 +16,7 @@ mongoose
   .then((res) => console.log("連線資料成功"));
 
 app.use(logger("dev"));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
